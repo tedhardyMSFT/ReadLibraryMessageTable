6abb6e98b371a4a327e35a4604b9e0448614d9a1
tedhardyMSFT <tedhardy@microsoft.com>
2016-06-24 09:19:45 -0700

    project skeleton

---
+++ ReadLibraryMessageTable/ReadLibraryMessageTable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace ReadLibraryMessageTable
+{
+    public class ReadMessageTable
+    {
+        public static Dictionary<string, string> EnumerateMessageTable(string LibraryPath)
+        {
+            Dictionary<string, string> Messages = new Dictionary<string, string>();
+
+            // TODO:MakeUseful
+
+            return Messages;
+        } // public static Dictionary<string,string> EnumerateMessageTable(
+    }
+}
