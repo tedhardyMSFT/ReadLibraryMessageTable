4a955b95db815ea5f086bc86d9499f0fbeb34dde
tedhardyMSFT <tedhardy@microsoft.com>
2016-06-24 09:22:32 -0700

    comment update with tasks

--- ReadLibraryMessageTable/ReadLibraryMessageTable.cs
+++ ReadLibraryMessageTable/ReadLibraryMessageTable.cs
@@ -13,7 +13,7 @@ namespace ReadLibraryMessageTable
         {
             Dictionary<string, string> Messages = new Dictionary<string, string>();
 
-            // TODO:MakeUseful
+            // TODO:MakeUseful - add code to load library, find resource, lock resources, load mesage table, enumerate and add to dictionary. Simple, eh?
 
             return Messages;
         } // public static Dictionary<string,string> EnumerateMessageTable(
